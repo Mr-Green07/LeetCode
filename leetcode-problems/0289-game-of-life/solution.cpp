4da5e7d0bdac53f06add49f1718b895adeb35912
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-02-06 18:56:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.6 MB (64.04%)

---
+++ leetcode-problems/0289-game-of-life/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<int>> dirs = {
+            {-1,-1},{-1,0},{-1,1},
+            {0,-1},        {0,1},
+            {1,-1},{1,0},{1,1}
+        };
+
+        // First pass
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = 0;
+
+                for (auto& d : dirs) {
+                    int x = i + d[0];
+                    int y = j + d[1];
+                    if (x >= 0 && x < m && y >= 0 && y < n &&
+                       (board[x][y] == 1 || board[x][y] == -1)) {
+                        liveNeighbors++;
+                    }
+                }
+
+                if (board[i][j] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3)
+                        board[i][j] = -1; // live → dead
+                } else {
+                    if (liveNeighbors == 3)
+                        board[i][j] = 2;  // dead → live
+                }
+            }
+        }
+
+        // Second pass
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = board[i][j] > 0 ? 1 : 0;
+            }
+        }
+    }
+};
+
