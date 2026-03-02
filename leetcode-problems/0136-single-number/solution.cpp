9726a62a43a4e16a28755dccaf431354728742a5
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-03-02 04:42:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (84.37%)

---
+++ leetcode-problems/0136-single-number/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // unordered_map<int, int> freq;
+        // for(int num: nums){
+        //     freq[num]++;
+        // }
+        // for(auto pair: freq){
+        //     if(pair.second == 1){
+        //         return pair.first;
+        //     }
+        // }
+        // return -1;
+        int result = 0;
+        for(int num: nums){
+            result ^= num;
+        }
+        return result;
+        
+
+    }
+};
