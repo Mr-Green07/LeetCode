7e9d75a3fdbd18bb6e468fbfe1f3ab50be4b9b76
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-03-04 19:30:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.7 MB (53.80%)

--- leetcode-problems/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ leetcode-problems/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,17 +1,24 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-    if (nums.empty()) return 0;
-
-        int i = 1;
-
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i - 1]) {
-                nums[i] = nums[j];
-                i++;
+        if (nums.empty()) return 0;
+        
+        int insertIndex = 1; // The Bouncer
+        
+        // The Scout loops from 1 to the end
+        for (int i = 1; i < nums.size(); i++) {
+            // Found a new unique element
+            if (nums[i] != nums[i - 1]) {
+                nums[insertIndex] = nums[i]; // Move it to the front
+                insertIndex++;               // Advance the insertion point
             }
         }
-
-        return i;        
+        
+        return insertIndex;
     }
 };
+
