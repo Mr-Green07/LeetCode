697d7d5405b6b738ce0fd3e2a5145de71a0f1c5f
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-07-08 02:51:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 52.4 MB (58.17%)

--- leetcode-problems/0055-jump-game/solution.cpp
+++ leetcode-problems/0055-jump-game/solution.cpp
@@ -1,30 +1,14 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int max_reach = 0;
-        int n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            // If the current index is beyond our furthest reach, we are stuck
-            if (i > max_reach) {
-                return false;
-            }
-            
-            // Update the furthest we can jump from here
-            max_reach = max(max_reach, i + nums[i]);
-            
-            // Optimization: If we can already reach the end, break early
-            if (max_reach >= n - 1) {
+        int target = nums.size() -1;
+        int maxR = 0;
+        for(int i = 0; i<= maxR; i++){
+            maxR = max(maxR, i + nums[i]);
+            if(maxR >= target){
                 return true;
             }
         }
-        
-        return true;
+        return false;
     }
 };
-
