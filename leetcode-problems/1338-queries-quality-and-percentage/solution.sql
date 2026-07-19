16f13ae15917fb5770531172211161831032b3f3
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-07-19 07:11:33 +0000

    Sync LeetCode submission Runtime - 877 ms (84.67%), Memory - 0.0B (100.00%)

---
+++ leetcode-problems/1338-queries-quality-and-percentage/solution.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+select
+query_name,
+round(avg(rating * 1.0 / position), 2) quality,
+round(sum(
+    case when rating<3 then 1 else 0 end
+    ) * 100.0 /count(*), 2
+) poor_query_percentage
+from Queries 
+group by query_name
+
