5b63d660abebb7ce459e3a85b5cf14d76c0e0e84
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-07-19 08:37:47 +0000

    Sync LeetCode submission Runtime - 637 ms (98.59%), Memory - 0.0B (100.00%)

---
+++ leetcode-problems/1882-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+select
+m.employee_id,
+m.name,
+count(e.reports_to) reports_count,
+round(avg(e.age * 1.0), 0) average_age
+from employees e
+join employees m
+on e.reports_to = m.employee_id
+group by m.employee_id, m.name
+order by m.employee_id;
