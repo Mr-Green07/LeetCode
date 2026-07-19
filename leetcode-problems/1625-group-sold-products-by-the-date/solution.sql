3b675c5d2930e3677d8b6394fe411354836e3c73
Yuvraj Singh <147185717+Mr-Green07@users.noreply.github.com>
2026-07-19 11:19:10 +0000

    Sync LeetCode submission Runtime - 732 ms (78.99%), Memory - 0.0B (100.00%)

---
+++ leetcode-problems/1625-group-sold-products-by-the-date/solution.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+select
+sell_date,
+count(product) as num_sold,
+string_agg(product, ',') within group(order by product asc) as products
+from (
+    select distinct sell_date, product
+    from activities
+) as s
+group by sell_date
+order by sell_date asc;
